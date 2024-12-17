97711942bc6dae54f957ebb809da5c17d25c41a5
Leonard-Bioinformatik2024 <leonard.hoffmann@stud.uni-frankfurt.de>
2024-12-17 18:49:24 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+"""This is an order and billing system for a restaurant.
+"""
+
+
+__author__ = "8532653, Hoffmann"
+__email__ = "leonard.hoffmann@stud.uni-frankfurt.de"
+
+
+# import buildin  # An example for builtins.
+
+# from numpy import array      # Another example for a third party module.
+
+# import my_modul            # An example for your own module.
+
+
+def main():
+    """This Funktion ties all modules together.
+    """
+
+    # You may put your code for initializing your program here, in case it is
+    # called as a stand-alone program.
+
+
+if __name__ == '__main__':
+    main()
