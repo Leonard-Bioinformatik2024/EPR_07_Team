4987bd26fe973a16cf7284199114da43ce758908
juliasingh58 <143778563+juliasingh58@users.noreply.github.com>
2024-12-20 00:55:42 +0530

    Update order.py

--- order.py
+++ order.py
@@ -9,19 +9,19 @@ __email__ = "leonard.hoffmann@stud.uni-frankfurt.de"
 class Order:
     """Contains methods to mark an order with an id.
     """
-    # fist order always recieves the id 1
+    # First order always recieves the id 1
     _id_counter = 1
 
     def __init__(self, product, special_requests=None):
         # id of the the order instance
         self.id = Order._id_counter
-        # next order will recieve an id one higher than the previouse
+        # Next order will recieve an id one higher than the previouse
         # with this the id will continually go up across all tables!
         Order._id_counter += 1
 
         self.product = product
 
-        # special_requests can be a list
+        # Special_requests can be a list
         if special_requests:
             self.special_requests = special_requests
         else:
@@ -30,7 +30,7 @@ class Order:
     def price(self):
         """Calculates the price including special requests.
         """
-        # counts who many strings of "extra" is contained in the list of special requests
+        # Counts how many strings of "extra" is contained in the list of special requests
         additions = sum(1 for request in self.special_requests if "extra" in request)
         return self.product.price + additions
 
